f3d0733ebf9fed036e9d5e8233e9cba8bcc4fa2f
Nathan Kuik <nkuik@toogoodtogo.com>
2021-03-08 21:51:20 +0100

    Do not raise raise error when no SECRET_KEY_BASE

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -11,12 +11,7 @@ config :hyf, Hyf.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = System.get_env("SECRET_KEY_BASE")
 
 config :hyf, HyfWeb.Endpoint,
   http: [
